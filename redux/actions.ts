47571a8bc6eac1d1140779adbc474c9630d62ebb
Imad Rahhali <imadrahhali5@gmail.com>
2024-05-31 18:42:18 +0100

    Update actions.ts

--- redux/actions.ts
+++ redux/actions.ts
@@ -3,15 +3,18 @@ export const ChangementDeLaRecherche = (requeteDeRecherche: string) => ({
   payload: requeteDeRecherche,
 });
 
+
 export const ChangementDeLaTaille = (taille: string) => ({
   type: 'ChangementDeLaTaille',
   payload: taille,
 });
 
+
 export const changeChargementState = (state: boolean) => ({
   type: 'changeChargementState',
   payload: state,
 });
+
 
 export const getResponce = (message: string) => ({
   type: 'getResponce',
